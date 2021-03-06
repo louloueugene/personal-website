b11f0818289dcc97a77017d520701305e2a312e7
Louis Eugene <leugene@microsoft.com>
2021-03-05 17:01:09 -0800

    Adding icons

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,6 +3,8 @@ import { graphql } from "gatsby";
 import { Helmet } from "react-helmet";
 import get from "lodash/get";
 import Layout from "../components/layout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
 
 const BlogPostTemplate = (props) => {
   const post = get(props, "data.contentfulBlogPost");
@@ -11,15 +13,25 @@ const BlogPostTemplate = (props) => {
   return (
     <Layout location={props.location}>
       <div style={{ maxWidth: 780 }}>
-        <Helmet title={`${post.title} | ${siteTitle}`} />
-
+        <Helmet title={`${post.title} | ${siteTitle}`}>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"
+          ></link>
+        </Helmet>
         <div className="wrapper">
           <h1 className="section-headline">{post.title}</h1>
+
           <p
             style={{
               display: "block",
             }}
           >
+            <FontAwesomeIcon
+              icon={faCalendarAlt}
+              size="1x"
+              style={{ marginRight: 7 }}
+            />
             {post.publishDate}
           </p>
 
